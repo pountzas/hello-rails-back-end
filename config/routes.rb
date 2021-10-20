8e131e46ff98b828978fecb25f2956539eec1746
pountzas <nikos@pountzas.gr>
2021-10-21 00:55:09 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
 end
